021bc4446454d67a16339a68ea7e05f223263c95
bilal <bilal.kureshi@iflair.com>
2025-11-24 11:47:04 +0530

    Initial Commit

---
+++ SWiftUIDemo/SWiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWiftUIDemoApp.swift
+//  SWiftUIDemo
+//
+//  Created by Yash Bhimani on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SWiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
